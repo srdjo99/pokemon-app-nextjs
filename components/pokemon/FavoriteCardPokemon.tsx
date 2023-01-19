8ae91e744ada71b01b083ca296b526aac039c3f3
Srdjan <srdjancoralic5@gmail.com>
2023-01-19 11:59:43 +0100

    add favorites pokemon ui

---
+++ components/pokemon/FavoriteCardPokemon.tsx
@@ -0,0 +1,20 @@
+import { Card, Grid } from "@nextui-org/react";
+import { FC } from "react";
+
+interface Props {
+  pokemonId: number;
+}
+
+export const FavoriteCardPokemon: FC<Props> = ({ pokemonId }) => {
+  return (
+    <Grid xs={6} sm={3} md={2} xl={1}>
+      <Card isHoverable isPressable css={{ padding: 10 }}>
+        <Card.Image
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`}
+          width={"100%"}
+          height={140}
+        />
+      </Card>
+    </Grid>
+  );
+};
--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
 import { NextPage } from "next";
+
 import { Layout } from "../../components/layouts";
+import { NoFavorites } from "../../components/ui";
+import { localFavorites } from "../../utils";
+import { FavoritePokemons } from "../../components/pokemon";
 
 const FavoritesPage: NextPage = () => {
+  const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+
+  useEffect(() => {
+    setFavoritePokemons(localFavorites.pokemons());
+  }, []);
+
   return (
     <Layout title="Pokemons - Favorites">
-      <h1>Favorites</h1>
+      {favoritePokemons.length === 0 ? (
+        <NoFavorites />
+      ) : (
+        <FavoritePokemons pokemons={favoritePokemons} />
+      )}
     </Layout>
   );
 };
